4d6c9fd669240a1b667c58ddc8d763ed6a595067
Gaurav Bhatnagar <gaurav.dilip.bhatnagar@maersk.com>
2019-08-25 10:54:26 +0530

    Adding some more comments.

--- app/src/main/java/com/itcse/workmanagersample/main/worker/rxworker/RxWorkerExample.kt
+++ app/src/main/java/com/itcse/workmanagersample/main/worker/rxworker/RxWorkerExample.kt
@@ -20,6 +20,26 @@ import io.reactivex.schedulers.Schedulers
 class RxWorkerExample(val context: Context, val workerParameters: WorkerParameters) :
     RxWorker(context, workerParameters) {
 
+
+    /**
+     * Override this method to define your actual work and return a {@code Single} of
+     * {@link androidx.work.ListenableWorker.Result} which will be subscribed by the
+     * {@link WorkManager}.
+     * <p>
+     * If the returned {@code Single} fails, the worker will be considered as failed.
+     * <p>
+     * If the {@link RxWorker} is cancelled by the {@link WorkManager} (e.g. due to a constraint
+     * change), {@link WorkManager} will dispose the subscription immediately.
+     * <p>
+     * By default, subscription happens on the shared {@link Worker} pool. You can change it
+     * by overriding {@link #getBackgroundScheduler()}.
+     * <p>
+     * An RxWorker is given a maximum of ten minutes to finish its execution and return a
+     * {@link androidx.work.ListenableWorker.Result}.  After this time has expired, the worker will
+     * be signalled to stop.
+     *
+     * @return a {@code Single<Result>} that represents the work.
+     */
     override fun createWork(): Single<Result> {
         return Observable.range(0, 100)
             .toList()
